2070a66a7f4750ff1dd572ea55ba10d249c364d0
kimMichel <kmichel.jae@gmail.com>
2022-07-07 22:09:29 -0300

    initial commit

---
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -0,0 +1,58 @@
+package co.tiagoaguiar.ganheinamega
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btn4Digit: Button = findViewById(R.id.btn4digit)
+        val btn6Digit: Button = findViewById(R.id.btn6digit)
+        val btn8Digit: Button = findViewById(R.id.btn8digit)
+
+        btn4Digit.setOnClickListener {
+
+            val digit = 4
+            passwordGenerator(digit, txtResult)
+        }
+        btn6Digit.setOnClickListener {
+
+            val digit = 6
+            passwordGenerator(digit, txtResult)
+        }
+        btn8Digit.setOnClickListener {
+
+            val digit = 8
+            passwordGenerator(digit, txtResult)
+        }
+    }
+
+    private fun passwordGenerator(digit: Int, txtResult: TextView) {
+
+        val numbers = mutableListOf<Int>()
+        val random = Random()
+
+        while(true) {
+            val number = random.nextInt(10)
+            numbers.add(number)
+
+            if (numbers.size == digit) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" ")
+
+    }
+}
